3201a1a8c269f6f9a5ce56654affb036d484c639
Gustavo <gustavo.farias16@gmail.com>
2025-07-22 19:44:01 -0300

    gustavo alteraçoes

--- chatbot.js
+++ chatbot.js
@@ -5,7 +5,13 @@ const axios = require('axios');
 const client = new Client({
     authStrategy: new LocalAuth()
 });
+//gustavo alteraçoes
+// Lista de palavras que ativam o bot
+const gatilhos = ['oi bot', 'olá', 'menu', 'agendar', 'ola', 'bom dia', 'boa tarde' ,'boa noite','tudo bem','salve','oi','suave'];
 
+// Usuários que ativaram o bot
+const usuariosAtivos = new Set();
+//gustavo alteraçoes
 client.on('qr', (qr) => {
     qrcode.generate(qr, { small: true });
     console.log('📌 Escaneie o QR Code para conectar.');
@@ -17,12 +23,29 @@ client.on('ready', () => {
 
 client.on('message', async msg => {
     const from = msg.from;
+    //gustavo alteraçoes
+    const texto = msg.body.trim().toLowerCase();
+
+    // Se a mensagem for uma palavra gatilho, ativa o bot para o usuário
+    if (gatilhos.includes(texto)) {
+        usuariosAtivos.add(from);
+        await msg.reply('👋 Ola sou assistente virtual Envie:\n1 - 📅Para agendar\n2 - 💈Ver Serviços\n3 - 📌Localização');
+        return;
+    }
+
+    // Se o usuário ainda não ativou o bot, ignorar mensagens
+    if (!usuariosAtivos.has(from)) {
+        return;
+    }
+
+    //gustavo alteraçoes
+    // Fluxo do bot
     if (msg.body === '1') {
         await msg.reply('Digite no formato: Nome,ID_do_Barbeiro,DataHora(YYYY-MM-DD HH:mm)');
     } else if (msg.body.includes(',') && msg.body.split(',').length === 3) {
         const [nome, barber_id, data_hora] = msg.body.split(',');
         try {
-            const res = await axios.post('http://localhost:3000/appointments', {
+            await axios.post('http://localhost:3000/appointments', {
                 barber_id,
                 cliente_nome: nome,
                 cliente_numero: from.replace('@c.us', ''),
@@ -34,8 +57,7 @@ client.on('message', async msg => {
             await msg.reply('❌ Erro ao criar agendamento.');
         }
     } else {
-        await msg.reply('Envie:\n1 - Para agendar');
-
+        await msg.reply('Envie:\n1 - Para agendar\n2 - Ver Serviços\n3 - Localização');
     }
 });
 
